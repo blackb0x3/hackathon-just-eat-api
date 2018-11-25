9b9266c68cdd4d4e0b065a69bc16e2dd500b8c52
Uthman Shaikh <32712826+uthman28@users.noreply.github.com>
2018-11-25 15:48:23 +0000

    Update urls.py

--- JustEat/JustEat/urls.py
+++ JustEat/JustEat/urls.py
@@ -26,7 +26,6 @@ router.register(r'requests', views.RequestsViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^idealweight/',views.IdealWeight),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
