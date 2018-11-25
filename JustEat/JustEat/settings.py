5650d33ec78cdaff506e48f430ee8a2a60787201
Uthman Shaikh <32712826+uthman28@users.noreply.github.com>
2018-11-25 14:35:22 +0000

    Update settings.py

--- JustEat/JustEat/settings.py
+++ JustEat/JustEat/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MySQLdb',
+    #'MySQLdb',
     'rest_framework',
     'Myapp',
     
